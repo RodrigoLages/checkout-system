6222601b26fdfd6e2091af54e5c6dfe3c1c51f27
Rodrigo Lages <rodrigolages@unifor.br>
2024-08-26 10:33:49 -0300

    Removed test route

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,6 @@ import { procedure, router } from "./trpc";
 import { productRouter } from "./router/product";
 
 export const appRouter = router({
-  getTest: procedure.query(async () => {
-    return [10, 20, 30];
-  }),
   product: productRouter,
 });
 

77e34cd1085746f7f73c1921d9e59cfd93b9c7cf
Rodrigo Lages <rodrigolages@unifor.br>
2024-08-28 10:55:50 -0300

    Blob token fix

--- util/deleteBlob.ts
+++ util/deleteBlob.ts
@@ -1,9 +1,11 @@
 import { del } from "@vercel/blob";
 
+const token = process.env.BLOB_READ_WRITE_TOKEN || process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN;
+
 export default async function deleteBlob(url: string | null): Promise<void> {
   if (!url) return console.log("URL não enviada");
   try {
-    await del(url, {token: process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN});
+    await del(url, { token });
   } catch (error) {
     console.error("Erro ao deletar a imagem:", error);
   }
--- util/sendImage.ts
+++ util/sendImage.ts
@@ -1,8 +1,10 @@
 import { put } from "@vercel/blob";
 
+const token = process.env.BLOB_READ_WRITE_TOKEN || process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN;
+
 export default async function sendImage(file: File): Promise<string | null> {
   try {
-    const newBlob = await put(file.name, file, {access: "public", token: process.env.NEXT_PUBLIC_BLOB_READ_WRITE_TOKEN})
+    const newBlob = await put(file.name, file, {access: "public", token })
 
     return newBlob.url;
   } catch (error) {

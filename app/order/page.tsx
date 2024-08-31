488c5c9d23565e3d2f59e8606e22cca20e710a99
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2024-08-31 17:18:53 -0300

    Added loading to pages

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import SideBar from "../_components/SideBar";
 import { trpc } from "../_trpc/client";
+import Loading from "../_components/Loading";
 
 export default function Order() {
   const getOrders = trpc.order.getAll.useQuery();
@@ -11,42 +12,46 @@ export default function Order() {
       <SideBar />
       <div className="flex-1 flex flex-col">
         <header className="h-16 bg-white border-b border-gray-300 flex items-center px-4"></header>
-        <div className="flex-1 p-4">
-          <div className="flex items-center p-4 space-x-5">
-            <h1 className="text-gray-800 text-2xl font-semibold">Vendas</h1>
+        {orders ? (
+          <div className="flex-1 p-4">
+            <div className="flex items-center p-4 space-x-5">
+              <h1 className="text-gray-800 text-2xl font-semibold">Vendas</h1>
+            </div>
+            <div className="w-full max-w-7xl mx-auto p-10 bg-[#ffffff]">
+              <div className="text-[#039ADC] text-2xl font-semibold mb-4">
+                Produtos
+              </div>
+              <div className="bg-[#e9e9e9] rounded-t-lg py-2 px-4 grid grid-cols-7 text-[#595959] text-sm font-semibold">
+                <span>Data</span>
+                <span>Produto</span>
+                <span>Cliente</span>
+                <span>Valor</span>
+                <span>Status</span>
+                <span>CPF</span>
+                <span>E-mail</span>
+              </div>
+              <div className="bg-white rounded-b-lg shadow-md border">
+                {orders?.map((order) => (
+                  <div className="grid grid-cols-7 py-2 px-4 border-b border-[#e0e0e0] text-[#595959] text-sm">
+                    <span>
+                      {new Date(order.createdAt).toLocaleDateString("pt-BR")}
+                    </span>
+                    <span>{order.product.name}</span>
+                    <span>{order.customerName}</span>
+                    <span>R$ {order.product.price.toFixed(2)}</span>
+                    <span>
+                      {order.status === "APPROVED" ? "Pago" : "Pendente"}
+                    </span>
+                    <span>{order.customerCPF}</span>
+                    <span>{order.customerEmail}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="w-full max-w-7xl mx-auto p-10 bg-[#ffffff]">
-            <div className="text-[#039ADC] text-2xl font-semibold mb-4">
-              Produtos
-            </div>
-            <div className="bg-[#e9e9e9] rounded-t-lg py-2 px-4 grid grid-cols-7 text-[#595959] text-sm font-semibold">
-              <span>Data</span>
-              <span>Produto</span>
-              <span>Cliente</span>
-              <span>Valor</span>
-              <span>Status</span>
-              <span>CPF</span>
-              <span>E-mail</span>
-            </div>
-            <div className="bg-white rounded-b-lg shadow-md border">
-              {orders?.map((order) => (
-                <div className="grid grid-cols-7 py-2 px-4 border-b border-[#e0e0e0] text-[#595959] text-sm">
-                  <span>
-                    {new Date(order.createdAt).toLocaleDateString("pt-BR")}
-                  </span>
-                  <span>{order.product.name}</span>
-                  <span>{order.customerName}</span>
-                  <span>R$ {order.product.price.toFixed(2)}</span>
-                  <span>
-                    {order.status === "APPROVED" ? "Pago" : "Pendente"}
-                  </span>
-                  <span>{order.customerCPF}</span>
-                  <span>{order.customerEmail}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
+        ) : (
+          <Loading />
+        )}
       </div>
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { trpc } from "./_trpc/client";
 import ProductCard from "./_components/ProductCard";
 import SideBar from "./_components/SideBar";
 import ProductModal from "./_components/ProductModal";
+import Loading from "./_components/Loading";
 
 export default function Home() {
   const getProducts = trpc.product.get.useQuery();
@@ -17,26 +18,30 @@ export default function Home() {
       <SideBar />
       <div className="flex-1 flex flex-col">
         <header className="h-16 bg-white border-b border-gray-300 flex items-center px-4"></header>
-        <div className="flex-1 p-4 overflow-y-auto">
-          <div className="flex items-center p-4 space-x-5">
-            <h1 className="text-gray-800 text-2xl font-semibold">Produtos</h1>
-            <button
-              onClick={() => setIsModalOpen(true)}
-              className="bg-blue-600 text-white text-sm font-semibold py-3 px-5 rounded-lg hover:bg-blue-700"
-            >
-              Criar
-            </button>
+        {products ? (
+          <div className="flex-1 p-4 overflow-y-auto">
+            <div className="flex items-center p-4 space-x-5">
+              <h1 className="text-gray-800 text-2xl font-semibold">Produtos</h1>
+              <button
+                onClick={() => setIsModalOpen(true)}
+                className="bg-blue-600 text-white text-sm font-semibold py-3 px-5 rounded-lg hover:bg-blue-700"
+              >
+                Criar
+              </button>
+            </div>
+            <div className="p-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {products?.map((product) => (
+                <ProductCard
+                  key={product.id}
+                  product={product}
+                  refetch={refetch}
+                />
+              ))}
+            </div>
           </div>
-          <div className="p-4 grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {products?.map((product) => (
-              <ProductCard
-                key={product.id}
-                product={product}
-                refetch={refetch}
-              />
-            ))}
-          </div>
-        </div>
+        ) : (
+          <Loading />
+        )}
       </div>
       <ProductModal
         isOpen={isModalOpen}

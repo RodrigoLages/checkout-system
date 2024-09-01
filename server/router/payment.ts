4984ea32f4971e9fd4fe48fa730e162bcf81d0a2
RodrigoLages <rodrigo_lagesbarbosa@hotmail.com>
2024-09-01 01:00:17 -0300

    Debug

--- server/router/payment.ts
+++ server/router/payment.ts
@@ -6,10 +6,18 @@ export const paymentRouter = router({
   webhook: procedure.input(z.any()).mutation(async ({ input }) => {
     const paymentId: string = input.data.id.toString();
     if (input.action === "payment.updated") {
-      prisma.order.update({
-        where: { paymentId },
-        data: { status: "APPROVED" },
-      });
+      console.log("entrou no if");
+      prisma.order
+        .update({
+          where: { paymentId },
+          data: { status: "APPROVED" },
+        })
+        .then((res) => {
+          console.log("pdate deu bom");
+        })
+        .catch((error) => {
+          console.log("update dum, ruim", error);
+        });
     }
     return;
   }),
